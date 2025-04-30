c3a15b250108e5a07a04020b523d2cd836a9019e
MarcosLabra <mlabra@gmail.com>
2025-04-30 12:50:16 -0300

    Actualizo info del sitio

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/R19-WEB/',  // 👈 el nombre de tu repo con las barras
   plugins: [react()],
 })
