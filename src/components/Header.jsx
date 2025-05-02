ab16ef16141996c3bbf75c59f38c1a8e64c182da
MarcosLabra <mlabra@gmail.com>
2025-05-02 09:02:32 -0300

    listo el menu desplegable

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,34 +12,32 @@ const Header = () => {
     };
 
     return (
-        <header>
-            <div className={styles.container} >
-                <div className={styles.subcontainer}>
-                    <div className={styles.header}>
-                        <h1 className={styles.title}>R19</h1>
-                        <HamburgerMenu
-                            isOpen={isOpen}
-                            menuClicked={toggleMenu}
-                            width={30}
-                            height={20}
-                            strokeWidth={4}
-                            rotate={0}
-                            color="#fbfbfb"
-                            borderRadius={2}
-                            animationDuration={0.3}
-                        />
-                    </div>
-                    <nav className={`${styles.menu} ${isOpen ? styles.open : styles.closed}`}>
-                        <ul>
-                            <li><a href="#tecnologia">Tecnología Educativa en Territorio</a></li>
-                            <li><a href="#dte">DTE</a></li>
-                            <li><a href="#equipo">Equipo</a></li>
-                            <li><a href="#redes">Redes y Testimonios</a></li>
-                        </ul>
-                    </nav>
+        <header className={styles.container}>
+            <div className={styles.subcontainer}>
+                <div className={styles.header}>
+                    <h1 className={styles.title}>R19</h1>
+                    <HamburgerMenu
+                        isOpen={isOpen}
+                        menuClicked={toggleMenu}
+                        width={30}
+                        height={20}
+                        strokeWidth={4}
+                        rotate={0}
+                        color="#fbfbfb"
+                        borderRadius={2}
+                        animationDuration={0.3}
+                    />
                 </div>
-                <h2 className={styles.subtitle}>Dirección de Tecnología Educativa</h2>
+                <nav className={`${styles.menu} ${isOpen ? styles.open : styles.closed}`}>
+                    <ul>
+                        <li><a href="#tecnologia">Tecnología Educativa en Territorio</a></li>
+                        <li><a href="#dte">DTE</a></li>
+                        <li><a href="#equipo">Equipo</a></li>
+                        <li><a href="#redes">Redes y Testimonios</a></li>
+                    </ul>
+                </nav>
             </div>
+            <h2 className={styles.subtitle}>Dirección de Tecnología Educativa</h2>
         </header>
     );
 };
